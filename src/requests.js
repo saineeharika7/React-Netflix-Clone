88e9f70f287b0e8292d7b6b2e430e23ad1be0473
neeharika <saineeharikaneha@gmail.com>
2021-01-13 12:06:05 +0530

    netflix-clone

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY ="ace076368e44b1d2533895815adc12ae"
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  };
+  
+  export default requests;
